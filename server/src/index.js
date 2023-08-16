d397fcb5892eb33dc36e70366d26c636f942690c
Dianne <mdianne.gabriel@gmail.com>
2023-08-16 10:46:32 -0400

    Require dotenv

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
-app.get('todos', (req, res) => {
-});
+app.get("todos", (req, res) => {});
 
 app.listen(8080);
