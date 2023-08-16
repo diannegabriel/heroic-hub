e4631e6a8290c19f4c294f7efd73c109abc37522
Dianne <mdianne.gabriel@gmail.com>
2023-08-16 10:53:23 -0400

    Set up morgan

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 dotenv.config();
 
 console.log(process.env.MONGO_URI)
 
 const app = express();
+
+app.use(morgan("tiny"));
 
 app.get("todos", (req, res) => {});
 
