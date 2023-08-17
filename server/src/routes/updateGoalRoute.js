a8de2410581b23a518188217a9857bdcc89b2a8b
Dianne <mdianne.gabriel@gmail.com>
2023-08-17 18:19:12 -0400

    Edit naming conventions

--- server/src/routes/updateGoalRoute.js
+++ server/src/routes/updateGoalRoute.js
@@ -1,13 +1,13 @@
 const GoalsModel = require("../models/Goals");
 
 module.exports = async (req, res) => {
-  const { id } = req.param;
+  const { id } = req.params;
   const goal = await GoalsModel.findById(id);
-  goal.goal = req.body.goal
-  goal.type = req.body.type
-  goal.description = req.body.description
-  goal.attribute = req.body.attribute
-  goal.status = req.body.status
+  goal.goal = req.body.goal;
+  goal.type = req.body.type;
+  goal.description = req.body.description;
+  goal.attribute = req.body.attribute;
+  goal.status = req.body.status;
   await goal.save();
   res.json(goal);
 };
