5c179faf46f3ffac8df42fecde0d5058f2556293
Dianne <mdianne.gabriel@gmail.com>
2023-08-17 18:04:18 -0400

    Remove in-line require in router

--- server/src/router.js
+++ server/src/router.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const isLoggedIn = require("./middleware/isLoggedIn");
 
+const usersRoute = require("./routes/usersRoute")
+const readGoalsRoute = require("./routes/readGoalsRoute");
+const createGoalRoute = require("./routes/createGoalRoute");
+const updateGoalRoute = require("./routes/updateGoalRoute");
+
 const router = express.Router();
 
-router.use("/auth", require("./routes/usersRoute"));
-router.get("/goals", isLoggedIn, require("./routes/readGoalsRoute"));
-router.post("/goals", isLoggedIn, require("./routes/createGoalRoute"));
+router.use("/auth", usersRoute);
+router.get("/goals", isLoggedIn, readGoalsRoute);
+router.post("/goals", isLoggedIn, createGoalRoute);
+router.put("/goals/:id", isLoggedIn, updateGoalRoute);
 
 module.exports = router;
