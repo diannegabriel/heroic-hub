427a2f7a1d814fe05bc72e9ba90256d9b840246b
Dianne <mdianne.gabriel@gmail.com>
2023-08-17 19:31:08 -0400

    Ensure goals are showing up. Yes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,27 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import readGoalsRequest from './api/readGoalsRequest'
+import { useState, useEffect } from "react";
+import "./App.css";
+import readGoalsRequest from "./api/readGoalsRequest";
 
 function App() {
   const [goals, setGoals] = useState([]);
-  
+
   useEffect(() => {
-    readGoalsRequest().then(allGoals => {
-      console.log(allGoals)
-    })
-  }, [])
+    readGoalsRequest().then(setGoals);
+  }, []);
 
   return (
-    <div>
-
+    <div className="App">
+      {goals.map((goal) => (
+        <div key={goal._id}>
+          {goal.goal}
+          {goal.description}
+          {goal.type}
+          {goal.attribute}
+          {goal.status}
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
