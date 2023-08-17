60bf981c1edca3e0d1160a02838faaa3bf489f58
Dianne <mdianne.gabriel@gmail.com>
2023-08-17 16:18:08 -0400

    Handle error

--- server/src/middleware/isLoggedIn.js
+++ server/src/middleware/isLoggedIn.js
@@ -3,9 +3,16 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res) => {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
+    // 401 error means token wasn't provided or malformed
     res.status(401).send("Invalid credentials");
   } else {
     const token = authHeader.split(" ")[1];
     console.log(token);
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        // 403 error means token has been provided but no access
+        res.status(403).send("Invalid credentials");
+      }
+    });
   }
 };
