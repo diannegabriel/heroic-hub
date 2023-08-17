b27b14e83bccd241c47f99c10efd7d53d09d1876
Dianne <mdianne.gabriel@gmail.com>
2023-08-17 19:36:02 -0400

    Import useQuery

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import "./App.css";
 import readGoalsRequest from "./api/readGoalsRequest";
+import { useQuery } from "react-query";
 
 function App() {
-  const [goals, setGoals] = useState([]);
+  // const [goals, setGoals] = useState([]);
+  const { isLoading, data: goals } = useQuery("todos", readGoalsRequest);
 
-  useEffect(() => {
-    readGoalsRequest().then(setGoals);
-  }, []);
+  // useEffect(() => {
+  //   readGoalsRequest().then(setGoals);
+  // }, []);
 
   return (
     <div className="App">
