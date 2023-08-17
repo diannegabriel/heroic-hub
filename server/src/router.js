8398d5a19b3df277aa270861997c4d03ba28b89b
Dianne <mdianne.gabriel@gmail.com>
2023-08-17 17:20:22 -0400

    Create another endpoint for goals - post

--- server/src/router.js
+++ server/src/router.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.use("/auth", require("./routes/usersRoute"));
 router.get("/goals", isLoggedIn, require("./routes/readGoalsRoute"));
+router.post("/goals", isLoggedIn, require("./routes/createGoalsRoute"));
 
 module.exports = router;
