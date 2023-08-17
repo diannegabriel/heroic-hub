a0353ce8d09fd294c46081f8e55ee463fb9360ba
Dianne <mdianne.gabriel@gmail.com>
2023-08-17 19:09:08 -0400

    Fix headers

--- client/src/api/readGoalsRequest.js
+++ client/src/api/readGoalsRequest.js
@@ -1,13 +1,14 @@
 const API_URL = `http://localhost:8080`;
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZGU2ZmMxZTYyZDQxYjZhYWExODU4YyIsImlhdCI6MTY5MjMwMjY2OH0.htz3GGWaPa4-aGOTBgIRkLuU8PG-nM4UNwnawYnWBbg";
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZGU2ZmMxZTYyZDQxYjZhYWExODU4YyIsImlhdCI6MTY5MjMwMjY2OH0.htz3GGWaPa4-aGOTBgIRkLuU8PG-nM4UNwnawYnWBbg";
 
 export default () => {
-  return fetch(`${API_URL}/todos`, {
-    method: 'GET',
+  return fetch(`${API_URL}/goals`, {
+    method: "GET",
     headers: {
-      Authorization: `Bearer ${token}`
-    }
-  })
-    .then (res => res.json())
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
 };
