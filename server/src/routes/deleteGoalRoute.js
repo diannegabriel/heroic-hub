63e1ffad62bc0f09e0c0ccb77ac0b620237b87d3
Dianne <mdianne.gabriel@gmail.com>
2023-08-17 18:27:16 -0400

    Finish deleteGoalRoute

--- server/src/routes/deleteGoalRoute.js
+++ server/src/routes/deleteGoalRoute.js
@@ -3,6 +3,6 @@ const GoalsModel = require("../models/Goals");
 module.exports = async (req, res) => {
   const { id } = req.params;
   const goal = await GoalsModel.findById(id);
-  await goal.save();
-  res.json(goal);
+  await goal.remove();
+  res.status(204).json(goal);
 };
