f09017d669cf81f17d31840f788635ecc849b69f
Dianne <mdianne.gabriel@gmail.com>
2023-08-17 14:23:07 -0400

    Add another endpoint: /auth

--- server/src/router.js
+++ server/src/router.js
@@ -3,5 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/login", require("./routes/loginRoute"));
+router.use("/auth", require("./routes/usersRoute"))
 
 module.exports = router;
