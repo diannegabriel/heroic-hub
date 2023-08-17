4f546b30df3f33f3336ea11424988434346b1a7f
Dianne <mdianne.gabriel@gmail.com>
2023-08-17 17:03:24 -0400

    Rename DailiesRoute to readDailiesRoute

--- server/src/router.js
+++ server/src/router.js
@@ -4,6 +4,6 @@ const isLoggedIn = require("./middleware/isLoggedIn");
 const router = express.Router();
 
 router.use("/auth", require("./routes/usersRoute"));
-router.get("/dailies", isLoggedIn, require("./routes/dailiesRoute"));
+router.get("/dailies", isLoggedIn, require("./routes/readDailiesRoute"));
 
 module.exports = router;
