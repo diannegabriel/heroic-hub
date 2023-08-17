ff5fe67c4e7212ac26157631cd6611c3c7afa377
Dianne <mdianne.gabriel@gmail.com>
2023-08-17 13:59:01 -0400

    Add endpoints

--- server/src/routes/usersRoute.js
+++ server/src/routes/usersRoute.js
@@ -1,1 +1,9 @@
+const express = require("express");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const router = express.Router();
+
+router.post("/register");
+
+router.post("/login");
