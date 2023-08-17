ada1451f8b901645a332b6fe3b36078cec91afa8
Dianne <mdianne.gabriel@gmail.com>
2023-08-17 19:00:36 -0400

    Hardcode auth token for now

--- client/src/api/readGoalsDefault.js
+++ client/src/api/readGoalsDefault.js
@@ -1,6 +1,13 @@
 const API_URL = `http://localhost:8080`;
 
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZGU2ZmMxZTYyZDQxYjZhYWExODU4YyIsImlhdCI6MTY5MjMwMjY2OH0.htz3GGWaPa4-aGOTBgIRkLuU8PG-nM4UNwnawYnWBbg";
+
 export default () => {
-  return fetch(`${API_URL}/todos`)
+  return fetch(`${API_URL}/todos`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
     .then (res => res.json())
 };
