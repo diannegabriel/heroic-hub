8d3f0ab6729759fbdbedd1bada876de0b6cc1998
Dianne <mdianne.gabriel@gmail.com>
2023-08-17 14:16:58 -0400

    Create user var

--- server/src/routes/usersRoute.js
+++ server/src/routes/usersRoute.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const UserModel = require("../models/Users.js");
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
   const { username, password } = req.body;
+  const user = await UserModel.findOne({ username });
 });
 
 router.post("/login");
