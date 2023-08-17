15a0e93ea48dbbdc62c983eae17429b16c3168f8
Dianne <mdianne.gabriel@gmail.com>
2023-08-17 18:11:17 -0400

    Complete updateGoalRoute

--- server/src/routes/updateGoalRoute.js
+++ server/src/routes/updateGoalRoute.js
@@ -3,5 +3,11 @@ const GoalsModel = require("../models/Goals");
 module.exports = async (req, res) => {
   const { id } = req.param;
   const goal = await GoalsModel.findById(id);
+  goal.goal = req.body.goal
+  goal.type = req.body.type
+  goal.description = req.body.description
+  goal.attribute = req.body.attribute
+  goal.status = req.body.status
+  await goal.save();
   res.json(goal);
 };
