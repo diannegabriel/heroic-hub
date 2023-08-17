fa4cdd5ea688df938d0eceeef4c26abecb28a260
Dianne <mdianne.gabriel@gmail.com>
2023-08-17 15:26:52 -0400

    Change JWT_TOKEN to JWT_SECRET

--- server/src/routes/usersRoute.js
+++ server/src/routes/usersRoute.js
@@ -34,7 +34,7 @@ router.post("/login", async (req, res) => {
     return res.json({ message: "Username or password is incorrect." });
   }
 
-  const token = jwt.sign({ id: user._id }, process.env.JWT_TOKEN);
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
   res.json({ token, userID: user._id });
 });
 
