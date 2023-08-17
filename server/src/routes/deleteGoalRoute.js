fdf7321f80d8b1ac99d3b5df93f2d5fa51251a20
Dianne <mdianne.gabriel@gmail.com>
2023-08-17 18:31:37 -0400

    Use deleteOne() instead of remove()

--- server/src/routes/deleteGoalRoute.js
+++ server/src/routes/deleteGoalRoute.js
@@ -3,6 +3,6 @@ const GoalsModel = require("../models/Goals");
 module.exports = async (req, res) => {
   const { id } = req.params;
   const goal = await GoalsModel.findById(id);
-  await goal.remove();
+  await goal.deleteOne();
   res.status(204).json(goal);
 };
