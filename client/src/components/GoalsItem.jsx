0e6f4ed13116fde6e38277f2e992d6cee5b4b121
Dianne <mdianne.gabriel@gmail.com>
2023-08-18 14:08:09 -0400

    Set up toggle status change

--- client/src/components/GoalsItem.jsx
+++ client/src/components/GoalsItem.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import { useQueryClient, useMutation } from "react-query";
+import updateGoalRequest from "../api/updateGoalRequest";
 
 const GoalsItem = ({ goal }) => {
-  const handleStatusChange = () => {};
+  const queryClient = new useQueryClient();
+
+  const { mutate: toggleStatusChange } = useMutation(() => {
+
+    return updateGoalRequest({ ...goal, status: 
+      goal.status === "complete" ? "incomplete"
+      : goal.status === "incomplete" ? "in progress"
+      : goal.status === "in progress" ? "complete" : ""
+    });
+  });
+
+  let statusIcon = "";
+  if (goal.status === "complete") {
+    statusIcon = <i className="nes-icon star"></i>;
+  } else if (goal.status === "incomplete") {
+    statusIcon = <i className="nes-icon star is-empty"></i>;
+  } else if (goal.status === "in progress") {
+    statusIcon = <i className="nes-icon star is-half"></i>;
+  }
   return (
     <div>
-      <button onClick={handleStatusChange}>
-        <i class="nes-icon is-large star is-empty" />
-      </button>
+      <button onClick={toggleStatusChange}>{statusIcon}</button>
       {goal.goal}
       {goal.description}
       {goal.type}
