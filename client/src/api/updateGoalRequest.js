ab96131fb2a6989c5999c8f4b79706ee999e9474
Dianne <mdianne.gabriel@gmail.com>
2023-08-18 13:06:43 -0400

    Complete updateGoalRequest

--- client/src/api/updateGoalRequest.js
+++ client/src/api/updateGoalRequest.js
@@ -3,12 +3,19 @@ const API_URL = `http://localhost:8080`;
 const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZGU2ZmMxZTYyZDQxYjZhYWExODU4YyIsImlhdCI6MTY5MjMwMjY2OH0.htz3GGWaPa4-aGOTBgIRkLuU8PG-nM4UNwnawYnWBbg";
 
-export default () => {
+export default (goal) => {
   return fetch(`${API_URL}/goals/${goal._id}`, {
-    method: "GET",
+    method: "PUT",
     headers: {
       Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
     },
+    body: JSON.stringify({
+      goal: goal.goal,
+      description: goal.description,
+      type: goal.type,
+      attribute: goal.attribute,
+      status: goal.status
+    })
   }).then((res) => res.json());
 };
